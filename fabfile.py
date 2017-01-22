d4d72c9929594f22dfd2514087338f84e47b8962
Jack <me@jack003.com>
2017-01-22 17:14:50 +0800

    edit fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,23 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import os
 
 from fabric.api import run, roles, cd
 from fabric.context_managers import env
 from fabric.contrib.project import rsync_project
 
 code_dir = '/jalpc'
+local_dir = '/Users/jack/Dropbox/Jack/jalpc-flask'
 exclude = ('.DS_Store', '*pyc', '.git', '.idea', '*sqlite3', 'migrations')
 
 env.roledefs = {
     'vps': ['root@vps.jack003.com']
 }
-
-
-@roles('vps')
-def deploy():
-    rsync_project(local_dir='.', remote_dir=code_dir, exclude=exclude)
-    run('uwsgi --reload /run/uwsgi.pid')
 
 
 @roles('vps')
@@ -29,3 +25,19 @@ def install():
         run('mv jalpc.conf /etc/nginx/sites-enabled/ && rm -f /etc/nginx/sites-enabled/default && service nginx reload')
         run('mkdir /home/www && chown -R www-data. /home/www')
         run('/usr/bin/uwsgi jalpc.ini')
+
+
+@roles('vps')
+def deploy():
+    rsync_project(local_dir='.', remote_dir=code_dir, exclude=exclude)
+    run('uwsgi --reload /run/uwsgi.pid')
+
+
+@roles('vps')
+def backup():
+    rsync_project(local_dir=local_dir, remote_dir=os.path.join(code_dir, 'migrations/versions'), upload=False, exclude=exclude)
+
+
+@roles('vps')
+def upload():
+    rsync_project(local_dir=os.path.join(local_dir, 'versions'), remote_dir=os.path.join(code_dir, 'migrations'), exclude=exclude)
